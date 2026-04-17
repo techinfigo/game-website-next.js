c2e9f861abee63b9dbdd00352a2ec9a338a9293b
Teachinfigo Team <thetechinfigo@gmail.com>
2026-04-17 15:00:39 +0000

    ignore eslint build error

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    // This allows the build to complete even if ESLint has internal errors
     ignoreDuringBuilds: true,
   },
 };
