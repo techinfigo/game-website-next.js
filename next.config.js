970e0f402a1c7e35bd006edca8e9297fc64c697e
Teachinfigo Team <thetechinfigo@gmail.com>
2026-04-18 09:58:35 +0000

    fixed next config for ES module

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
