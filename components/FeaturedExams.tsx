26d079d7fe803388bb5c65d52b946f3a9ac8279f
Teachinfigo Team <thetechinfigo@gmail.com>
2026-05-21 17:11:12 +0530

    Update FeaturedExams.tsx

--- components/FeaturedExams.tsx
+++ components/FeaturedExams.tsx
@@ -92,7 +92,7 @@ const FeaturedExams: React.FC<FeaturedExamsProps> = ({ onNavigate }) => {
       icon: Briefcase,
       color: 'text-emerald-400',
       image: "/exams/psu-bg.jpg",
-      logo: "/exams/psu-logo.jpg",
+      logo: "/exams/logo-bhel.png",
       action: 'psu'
     },
     {
